4e336a9454abd8690fbdbaeeccb1043e7e1f21e5
Tim <github@timgreen.ws>
2019-06-09 18:28:50 +1000

    added new footer into includes

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+<footer class="idle_footer">
+   <div class="container">
+     <div class="row">
+         <div class="col-lg-12">
+            <div class="idle_footer_text text-center">
+             <p>Copyright Idle. <?php echo date("Y");?></p>
+           </div>
+         </div>
+       </div>
+    </div>
+</footer>
