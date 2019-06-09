9efdbefb016b857a3051decf215f73245b919b57
Tim <github@timgreen.ws>
2019-06-09 15:38:22 +1000

    updated gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,11 +3,11 @@ module.exports = function(grunt){
     grunt.initConfig({
       concat: {
         js: {
-          src: ['assets/js/jquery.js', 'assets/js/pace.js', 'assets/js/wow.min.js', 'assets/js/slick.min.js', 'assets/js/typed.min.js','assets/js/main.js'],
+          src: ['assets/js/jquery.min.js', 'assets/js/modernizr.min.js', 'assets/js/bootstrap/bootstrap.min.js', 'assets/js/bootstrap/popper.min.js', 'assets/js/slick/slick.min.js', 'assets/js/jquery.meanmenu.js', 'assets/js/jquery.magnific-popup.min.js', 'assets/js/wow.min.js', 'assets/js/idle-script.js'],
           dest: 'build/js/scripts.js',  
         },
         css: {
-          src: ['assets/css/fonts.css', 'assets/css/style.css', 'assets/css/slick.css'],
+          src: ['assets/css/bootstrap/bootstrap.min.css', 'assets/css/animate.css', 'assets/css/meanmenu.css','assets/css/slick/slick.css','assets/css/slick/slick-theme.css','assets/css/site.css','assets/css/idle-styles.css','assets/css/responsive.css'],
           dest: 'build/css/styles.css',
         },
       },
