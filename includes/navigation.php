cf3b356551bc71bd6ab2d6c886e768f6e20ce95b
Tim <github@timgreen.ws>
2019-06-10 13:39:11 +1000

    added new menu link

--- includes/navigation.php
+++ includes/navigation.php
@@ -4,6 +4,8 @@
         <li><a href="#feature" class="page_scroll">Features</a></li>
         <li><a href="#pricing" class="page_scroll">pricing</a></li>
         <li><a href="#download" class="page_scroll">Download</a></li>
+        <li><a href="#testimonials" class="page_scroll">Testimonials</a></li>
+        
         <li><a href="#blog" class="page_scroll">Blog<span class="caret"><i class="fas fa-angle-down"></i></span></a>
             <ul class="sub-menu">
             <li><a href="blog.php">blog</a></li>
