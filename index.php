02e2423060741859ccb58326ac5c094b96685ebb
Tim <github@timgreen.ws>
2019-06-17 00:21:05 +1000

    update site title

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en" >
 <head>
-  <title>Idle</title>
+  <title>Idle - Work In Progress</title>
   <meta charset="utf-8">
   <meta name="description" content="">
   <meta name="robots" content="noindex, nofollow, all" />
