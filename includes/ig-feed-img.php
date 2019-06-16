32e4d7bf43143c3ae3a8ffb39b59554b9c35d5b1
Tim <github@timgreen.ws>
2019-06-16 18:08:14 +1000

    added one ig post image from SWC

---
+++ includes/ig-feed-img.php
@@ -0,0 +1,1 @@
+https://scontent-sjc3-1.cdninstagram.com/vp/9e92eac31e792fb732feacabfcb63326/5DC40E08/t51.2885-15/e15/c135.0.450.450a/62256160_316030485987156_7606320601380163574_n.jpg?_nc_ht=scontent-sjc3-1.cdninstagram.com
