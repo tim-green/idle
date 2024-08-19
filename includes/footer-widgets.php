103b34c69fca23528875deed46aa3d5563ec44f6
Tim <github@timgreen.ws>
2024-08-19 10:34:40 +1000

    updated twitter icon to twitter/x icon

--- includes/footer-widgets.php
+++ includes/footer-widgets.php
@@ -12,7 +12,7 @@
                             <div class="widget-link">
                                 <ul>
                                     <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-x-twitter"></i></a></li>
                                     <li><a href="#"><i class="fab fa-instagram"></i></a></li>
                                     <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
                                 </ul>
